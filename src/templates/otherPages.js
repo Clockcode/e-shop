38e8535406ec0401aa296f9a8cea64f96f2efbf5
sowasred <muldurozan@gmail.com>
2020-05-26 00:52:21 -0400

    menu other pages updated

--- src/templates/otherPages.js
+++ src/templates/otherPages.js
@@ -1,12 +1,65 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 
+import Header from "../components/header"
+import Footer from "../components/footer"
+import headerStyle from "../components/styles/header.module.scss"
+
 import Layout from "../components/layout"
+
+export const query = graphql`
+  query($slug: String!) {
+    strapiMenuOtherPages(slug: { eq: $slug }) {
+      Title
+      PageContent
+      slug
+    }
+    strapiHomePage {
+      NavigationMenu {
+        categories {
+          Title
+          Description
+          slug
+        }
+        menu_other_pages {
+          Title
+          slug
+          PageContent
+        }
+      }
+      Footer {
+        FoooterText
+        footer_company_pages {
+          Title
+          PageContent
+          slug
+        }
+        footer_help_pages {
+          PageContent
+          Title
+          slug
+        }
+      }
+    }
+  }
+`
+
 const otherPages = ({ data }) => {
   return (
     <React.Fragment>
       <Layout>
-        <h1>Derry Store</h1>
+        <Header
+          categories={data.strapiHomePage.NavigationMenu.categories}
+          otherPages={data.strapiHomePage.NavigationMenu.menu_other_pages}
+          className={headerStyle.navigation}
+        />
+        <h1>{data.strapiMenuOtherPages.Title}</h1>
+        <p>{data.strapiMenuOtherPages.PageContent}</p>
+        <Footer
+          footerText={data.strapiHomePage.Footer.FooterText}
+          helpPages={data.strapiHomePage.Footer.footer_help_pages}
+          companyPages={data.strapiHomePage.Footer.footer_company_pages}
+        />
       </Layout>
     </React.Fragment>
   )
